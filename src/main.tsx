e8fe3632dde70f99f10150e84286db68fd2c3b5a
Md. Rahat Mahmud <mdayatmahmud.101@gmail.com>
2026-08-20 15:36:25 +0600

    Fix: add BrowserRouter basename for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import "./styles/utilities.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.PROD ? "/Webmotion.Studio" : ""}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
