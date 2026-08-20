642c0b4cecf62a5fd787f787c12c554b334cd71e
Md. Rahat Mahmud <mdayatmahmud.101@gmail.com>
2026-08-20 12:15:08 +0600

    Fix GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import "./styles/utilities.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Webmotion-Studio-black-and-red-">
       <App />
     </BrowserRouter>
   </React.StrictMode>
